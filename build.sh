5588cc41969e9c09e05330ce5f618babc4b4b74a
an.herip <80059395+pembotol@users.noreply.github.com>
2024-06-07 08:41:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -168,9 +168,10 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
+log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach ReVanced YouTube"
 log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "$(cat $TEMP_DIR/*-rv/changelog.md)"
+log "\n[MainRepo](https://github.com/Jhosua910/revanced-extended-anddea)\n"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
